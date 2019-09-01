381782a81336c2cf387ed8177ac7bc1d59386d89
jeff <jeffvp@protonmail.ch>
2019-08-31 23:58:43 -0400

    Set up client app.  Will be building this instead of "hypothesizing" what sort of actions I'll need to build for on the server.  As I build functionality on the client I'll build associated functionality on the server.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <span class="headline text-uppercase">WW</span>
+        <span class="font-weight-light text-capitalize subtitle-1"
+          >Points Plus Tracking
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <span v-on="on">*</span>
+            </template>
+            <span>
+              This site is in no way affiliated with WW (Weight Watchers). This
+              is purely for personal use.
+            </span>
+          </v-tooltip>
+        </span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
@@ -15,6 +26,13 @@
         <span class="mr-2">Latest Release</span>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app clipped v-model="drawer">
+      <ul>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+      </ul>
+    </v-navigation-drawer>
 
     <v-content>
       <router-view></router-view>
@@ -23,15 +41,11 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+  name: "App",
+  components: {},
   data: () => ({
-    //
-  }),
+    drawer: true
+  })
 };
 </script>
---
+++ client/src/views/Food.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Food"
+};
+</script>
+
+<style scoped></style>
