0a64737e252f0b4c189daf56ab7ea666c97b2fc9
jeff <jeffvp@protonmail.ch>
2019-09-02 11:29:35 -0400

    Added vue apollo to client to build API calls

--- client/src/router.js
+++ client/src/router.js
@@ -19,8 +19,17 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: () => import(/* webpackChunkName: "about" */ "./views/About")
+    },
+    {
+      path: "/day/:day",
+      name: "Day",
+      component: () => import("./views/Day")
+    },
+    {
+      path: "/apollo",
+      name: "Apollo",
+      component: () => import("./components/ApolloExample")
     }
   ]
 });
--- client/src/views/Day.vue
+++ client/src/views/Day.vue
@@ -1,11 +1,42 @@
 <template>
-  <div></div>
+  <v-container fluid class="day-container">
+    {{ day }}
+    <ul>
+      <li v-for="period in periods" :key="period._id">
+        {{ period._id }} -> {{ period.timePeriod }}
+      </li>
+    </ul>
+  </v-container>
 </template>
 
 <script>
+import PERIODS from "../graphql/Periods.gql";
+
 export default {
-  name: "Day"
+  name: "Day",
+  data: () => ({
+    periods: [{}]
+  }),
+  computed: {
+    day() {
+      return this.$route.params.day;
+    }
+  },
+  apollo: {
+    periods: {
+      query: PERIODS,
+      update: data => data.periods
+    }
+  },
+  mounted() {
+    this.$apollo.queries.periods;
+    console.log("this.periods", this.periods);
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.day-container {
+  border: 1px solid black;
+}
+</style>
